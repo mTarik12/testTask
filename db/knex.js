500e160c6dc1c20dc38ad9f5582cfd59cd92681b
mTarik12 <72448312+mTarik12@users.noreply.github.com>
2021-02-06 18:14:27 +0200

    deleted over methods in knex

--- db/knex.js
+++ db/knex.js
@@ -13,7 +13,6 @@ async function createSchema() {
 
     if (!await knex.schema.hasTable('users')) {
         knex.schema.createTable('users', (table) => {
-            table.increments('_id');
             table.string('id');
             table.string('first_name');
             table.string('last_name');
@@ -26,18 +25,9 @@ async function createSchema() {
                 table.string('date');
                 table.string('page_views');
                 table.string('clicks');
-                table
-                    .integer('id')
-                    .unsigned()
-                    .references('users._id');
             })
             .then(() => knex('users').insert(allUsers))
-            .then(rows => knex('users_statistics').insert(usersStatistics, { id: rows[0] }))
-            .then(() => {
-                knex('users')
-                    .join('users_statistics', 'id', 'users_statistics.id')
-                    .select('users.firs_name as user', 'users_statistics.user_id as id')
-            });
+            .then(() => knex('users_statistics').insert(usersStatistics));
     }
 }
 
